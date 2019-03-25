71f97b89c8e76b7cb57c45621dff4916ed83a063
Poonam Joshi <31898841+Flyingweavy@users.noreply.github.com>
2019-03-26 00:16:54 +0530

    Add files via upload

--- app/src/main/java/com/mindorks/app/MainActivity.kt
+++ app/src/main/java/com/mindorks/app/MainActivity.kt
@@ -2,6 +2,10 @@ package com.mindorks.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.TextView
+import com.mindorks.RadialProgressBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        progress.setProgressValues(70, 90, 90)
-        progress.hasTwoProgressView(true)
 
-//        ahvajggj
+         progress.setProgressValues(70, 90, 90)
+            txtvStatusCircle.text= String.format("Outer : %d",progress.getOuterProgress())
+
+
+
+
     }
 }
+
